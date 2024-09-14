d0ed4c6965deceba1444dd23eba96e87501c44b9
mallickarif <mallickarif454@gmail.com>
2024-09-15 03:38:52 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App";
+// import App from "./App";
 // import App from './App.jsx'
 import "./index.css";
+import viteLogo from "/vite.svg";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <img src='{viteLogo}' alt='vite logo' />
   </React.StrictMode>,
 )
